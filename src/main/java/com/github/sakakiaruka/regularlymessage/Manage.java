cd1e0a2e5db74e9872ce20889335afe3322b8e03
Aruka <yshida1pc7@gmail.com>
2023-03-20 22:00:23 +0900

    update : add tabCompleter

--- src/main/java/com/github/sakakiaruka/regularlymessage/Manage.java
+++ src/main/java/com/github/sakakiaruka/regularlymessage/Manage.java
@@ -3,13 +3,16 @@ package com.github.sakakiaruka.regularlymessage;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
 import static com.github.sakakiaruka.regularlymessage.SettingsLoad.*;
 
-public class Manage implements CommandExecutor {
+public class Manage implements CommandExecutor, TabCompleter {
     @Override
     public boolean onCommand(CommandSender sender, Command command,String label,String[] args){
         /*
@@ -39,5 +42,16 @@ public class Manage implements CommandExecutor {
         }
 
         return true;
+    }
+
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        if(!(sender instanceof Player))return null;
+        if(args.length != 1)return null;
+        List<String> list = Arrays.asList("add","remove");
+        if(!sender.isOp())return list;
+        list.add("reload");
+        return list;
     }
 }
